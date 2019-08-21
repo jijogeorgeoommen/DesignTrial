ae487ae2b2466ba40924bdc1f70ee9b7b25f1528
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-08-21 11:02:48 +0530

    Initial Commit

---
+++ DesignTrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignTrial
+//
+//  Created by JIJO G OOMMEN on 21/08/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
